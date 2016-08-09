b61af40559c370fd803e38dc067d40ab036501ef
18817592677 <502287658@qq.com>
2016-08-09 17:02:12 +0800

    first commit

---
+++ Desktop/测试/测试/测试/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  测试
+//
+//  Created by qianfeng on 16/8/9.
+//  Copyright © 2016年 DDD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
